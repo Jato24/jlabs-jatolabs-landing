e3eed03b879ae28abaef134ce9d2154f2e10f29e
Mike Jato <miguel@jetz.app>
2026-08-04 08:55:49 -0600

    Update experience section and enhance technical lead presentation in HTML; correct email in site configuration

--- site-config.js
+++ site-config.js
@@ -2,7 +2,7 @@ window.SITE_CONFIG = Object.freeze({
   brandName: 'Jato Labs',
   founderName: 'Miguel Jato',
   domain: 'https://jatolabs.tech',
-  email: 'miguel@jatolabs.tech',
+  email: 'hola@jatolabs.tech',
   phone: '+52 55 1502 4056',
   location: 'México / Remoto',
   linkedinUrl: 'https://www.linkedin.com/in/jatomx/',
